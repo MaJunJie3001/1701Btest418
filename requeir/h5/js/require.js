9084301cfac32091c33e9b809996e06a42b22c32
MaJunJie3001 <majunjie_3001@163.com>
2019-04-18 09:05:12 +0800

    第一次

---
+++ requeir/h5/js/require.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: 马俊杰 
+ * @Date: 2019-04-18 08:59:48 
+ * @Last Modified by: 马俊杰
+ * @Last Modified time: 2019-04-18 09:04:05
+ */
+//引入模块
+function require(arr, callback) {
+    var arr = [];
+
+}
+
+
+//定义模块
+function define() {
+    return
+}
